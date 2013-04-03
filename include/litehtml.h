967cac69ba9ccbd09c301027e6f4c92b8d651e64
Hiroyuki-Nagata <newserver002@gmail.com>
2013-04-03 23:26:44 +0900

    Add wxWidgets check to configure.ac

---
+++ include/litehtml.h
@@ -0,0 +1,9 @@
+#ifndef _LITEHTML_H_
+#define _LITEHTML_H_
+
+#include "../litehtml/html.h"
+#include "../litehtml/document.h"
+#include "../litehtml/element.h"
+#include "../litehtml/stylesheet.h"
+
+#endif /** _LITEHTML_H_ */
